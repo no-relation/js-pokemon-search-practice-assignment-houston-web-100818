338ff487801d297ec8771e85995c4c313984bfa0
no-relation <eddiec76@gmail.com>
2018-11-26 16:02:01 -0600

    put script tags in right place

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
-  //YOUR CODE HERE
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log(POKEMON)
+//   //YOUR CODE HERE
+// })
+
+const API = 'http://localhost:3000/pokemon';
+const pokeSearch = document.getElementById('pokemon-search-input');
+const pokeSubmit = document.getElementById('pokemon-search-submit');
+const pokeTainer = document.getElementById('pokemon-container');
+
+fetch(API)
+  .then((resp) => resp.json())
+  .then(function (data) {
+    console.log("this is data ", data)
+  })
+
+// console.log("this is pokesubmit", pokeSubmit)
+
+pokeSearch.addEventListener('keydown', function(e){
+
+  console.log(e.which)  
 })
+console.log("this is pokesubmit", pokeSubmit)
