4714c65d9d7ae540d16e61bd01933d481af82c77
no-relation <eddiec76@gmail.com>
2018-11-27 09:28:17 -0600

    search fixed

--- src/index.js
+++ src/index.js
@@ -19,7 +19,8 @@ fetch(API)
   })
 
   
-pokeSubmit.addEventListener('click', function() {
+pokeSubmit.addEventListener('click', function(e) {
+  e.preventDefault()
   let newPokArray = pokArray.filter(function(pokemon){
     return pokemon.name.includes(pokeSearch.value)
   })
@@ -30,10 +31,11 @@ pokeReset.addEventListener('click', function(){
   render(pokArray)
 })
 
-function render(pokArray) {  
-  if (pokArray.length != 0){
-    pokArray.forEach(function(pokemon){
-      pokemonItem = 
+function render(renderArray) {  
+  pokeTainer.innerHTML = ''
+  if (renderArray.length != 0){
+    renderArray.forEach(function(pokemon){
+      let pokemonItem = 
       `<div class="pokemon-container">
         <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc" class="pokemon-frame">
           <h1 class="center-text">${capitalizer(pokemon.name)}</h1>
@@ -42,15 +44,35 @@ function render(pokArray) {
               <img data-id="${pokemon.id}" data-action="flip" class="toggle-sprite" src="${pokemon.sprites.front}">
             </div>
           </div>
-        </div>
+        </div>  
       </div>`
       pokeTainer.insertAdjacentHTML('beforeend', pokemonItem)
+      // let pokeImg = document.querySelector('.toggle-sprite')
+      // pokeImg.addEventListener('click',function(e){
+      //   if (e.target.src === pokemon.sprites.front){
+      //     e.target.src = pokemon.sprites.back
+      //   } else {
+      //     e.target.src = pokemon.sprites.front
+      //   }
+      // })
     })
   } else {
     pokemonItem = "<p><center>There are no Pokémon here</center></p>"
     pokeTainer.insertAdjacentHTML('beforeend', pokemonItem)
   }
 }
+
+// shamelessly stolen from demo page
+document.getElementById('pokemon-container').addEventListener('click', event => {
+  if (event.target.dataset.action === 'flip') {
+    const targetPoke = POKEMON.find(pokeObj => pokeObj.id == event.target.dataset.id)
+    if (event.target.src === targetPoke.sprites.front) {
+      event.target.src = targetPoke.sprites.back
+    } else {
+      event.target.src = targetPoke.sprites.front
+    }
+  }
+})
 
 function capitalizer(name){
   initial = name[0].toUpperCase()
