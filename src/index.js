3225faa1cf2b8a27cb1bf040612612f24478d965
no-relation <eddiec76@gmail.com>
2018-11-26 20:30:04 -0600

    renders out the way I want

--- src/index.js
+++ src/index.js
@@ -8,16 +8,37 @@ const pokeSearch = document.getElementById('pokemon-search-input');
 const pokeSubmit = document.getElementById('pokemon-search-submit');
 const pokeTainer = document.getElementById('pokemon-container');
 
+let pokArray = []
+
 fetch(API)
   .then((resp) => resp.json())
-  .then(function (data) {
-    console.log("this is data ", data)
+  .then(function(data) {
+    pokArray = data
+    render(pokArray)
   })
 
-// console.log("this is pokesubmit", pokeSubmit)
-
-pokeSearch.addEventListener('keydown', function(e){
-
-  console.log(e.which)  
+pokeSubmit.addEventListener('click', function() {
+  render(pokArray)
 })
-console.log("this is pokesubmit", pokeSubmit)
+
+function render(pokArray) {  
+  pokArray.forEach(function(pokemon){
+    pokemonItem = 
+    `<div class="pokemon-container">
+      <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc" class="pokemon-frame">
+        <h1 class="center-text">${capitalizer(pokemon.name)}</h1>
+        <div style="width:239px;margin:auto">
+          <div style="width:96px;margin:auto">
+            <img data-id="${pokemon.id}" data-action="flip" class="toggle-sprite" src="${pokemon.sprites.front}">
+          </div>
+        </div>
+      </div>
+    </div>`
+    pokeTainer.insertAdjacentHTML('beforeend', pokemonItem)
+  })
+}
+
+function capitalizer(name){
+  initial = name[0].toUpperCase()
+  return initial + name.slice(1)
+}
